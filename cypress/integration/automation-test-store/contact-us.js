99ec68611ec9c1f31dda2da64502ad29524be442
alexandrup <alexandrup@ensemble.com>
2022-05-25 19:09:06 +0300

    Refactored test files using Selectors Fixture

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -2,20 +2,19 @@
 
 describe('Test Contact Us form via Automation Test Store', () => {
   beforeEach(() => {
-      cy.fixture('contactUs').as('contactUsData')
-
-      cy.visit('https://www.automationteststore.com/')
-      cy.xpath("//a[contains(@href, 'contact')]").click()
+    cy.fixture('./automation-test-store/selectors').as('selectors')
+    cy.fixture('contact_us_data').as('contact_us_data')
+    cy.visit('https://automationteststore.com/index.php?rt=content/contact')
   })
 
   it('Should be able to submit a successful submission via contact us form', function () {
-    cy.get('#ContactUsFrm_first_name')
-      .type(this.contactUsData.first_name),
-    cy.get('#ContactUsFrm_email')
-      .type(this.contactUsData.email)
-    cy.get('#ContactUsFrm_enquiry')
-      .type(this.contactUsData.comment)
-    cy.get("button[title='Submit']")
+    cy.get(this.selectors.contact_us_page.first_name_field)
+      .type(this.contact_us_data.first_name),
+    cy.get(this.selectors.contact_us_page.email_field)
+      .type(this.contact_us_data.email)
+    cy.get(this.selectors.contact_us_page.enquiry_field)
+      .type(this.contact_us_data.comment)
+    cy.get(this.selectors.contact_us_page.submit_cta)
       .click()
-  });
-});
+  })
+})
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,33 +1,33 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact US form via WebdriverUni', () => {
-    beforeEach(() => {
-        cy.fixture('contactUs').as('contactUsData')
+  beforeEach(() => {
+    cy.fixture('./web-driver-university/selectors').as('selectors')
+    cy.fixture('contact_us_data').as('contact_us_data')
+    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+  })
 
-        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-    })
+  it('Should be able to submit a successful submission via contact us form', function () {
+    cy.get(this.selectors.contact_us_page.first_name_field)
+      .type(this.contact_us_data.first_name),
+    cy.get(this.selectors.contact_us_page.last_name_field)
+      .type(this.contact_us_data.last_name)
+    cy.get(this.selectors.contact_us_page.email_field)
+      .type(this.contact_us_data.email)
+    cy.get(this.selectors.contact_us_page.comments_field)
+      .type(this.contact_us_data.comment)
+    cy.get(this.selectors.contact_us_page.submit_cta)
+      .click()
+  })
 
-    it('Should be able to submit a successful submission via contact us form', function () {
-        cy.get('[name="first_name"]')
-          .type(this.contactUsData.first_name),
-        cy.get('[name="last_name"]')
-          .type(this.contactUsData.last_name)
-        cy.get('[name="email"]')
-          .type(this.contactUsData.email)
-        cy.get('textarea.feedback-input')
-          .type(this.contactUsData.comment)
-        cy.get('[type="submit"]')
-          .click()
-    });
-
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', function () {
-        cy.get('[name="first_name"]')
-          .type(this.contactUsData.first_name),
-        cy.get('[name="last_name"]')
-          .type(this.contactUsData.last_name)
-        cy.get('textarea.feedback-input')
-          .type(this.contactUsData.comment)
-        cy.get('[type="submit"]')
-          .click()
-    })
-});
+  it('Should not be able to submit a successful submission via contact us form as all fields are required', function () {
+    cy.get(this.selectors.contact_us_page.first_name_field)
+      .type(this.contact_us_data.first_name),
+    cy.get(this.selectors.contact_us_page.last_name_field)
+      .type(this.contact_us_data.last_name)
+    cy.get(this.selectors.contact_us_page.comments_field)
+      .type(this.contact_us_data.comment)
+    cy.get(this.selectors.contact_us_page.submit_cta)
+      .click()
+  })
+})
