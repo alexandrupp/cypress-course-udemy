961501d0c7824d7ce5f24fad34834a9a03c63f7c
alexandrup <alexandrup@ensemble.com>
2022-05-25 14:19:08 +0300

    Created 2 tests Fixtures. Visit. BaseUrl

--- cypress/integration/contactUs.js
+++ cypress/integration/contactUs.js
@@ -1,11 +1,33 @@
 /// <reference types="Cypress" />
 
 describe('Test Contact US form via WebdriverUni', () => {
-    it('Should be able to submit a successful submission via contact us form', () => {
-        cy.visit('http://webdriveruniversity.com')
+    beforeEach(() => {
+        cy.fixture('contactUs').as('contactUsData')
+
+        cy.visit('/Contact-Us/contactus.html')
+    })
+
+    it('Should be able to submit a successful submission via contact us form', function () {
+        cy.get('[name="first_name"]')
+          .type(this.contactUsData.first_name),
+        cy.get('[name="last_name"]')
+          .type(this.contactUsData.last_name)
+        cy.get('[name="email"]')
+          .type(this.contactUsData.email)
+        cy.get('textarea.feedback-input')
+          .type(this.contactUsData.comment)
+        cy.get('[type="submit"]')
+          .click()
     });
 
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
-        
-    });
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', function () {
+        cy.get('[name="first_name"]')
+          .type(this.contactUsData.first_name),
+        cy.get('[name="last_name"]')
+          .type(this.contactUsData.last_name)
+        cy.get('textarea.feedback-input')
+          .type(this.contactUsData.comment)
+        cy.get('[type="submit"]')
+          .click()
+    })
 });
