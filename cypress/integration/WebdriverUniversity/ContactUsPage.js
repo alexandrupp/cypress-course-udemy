ecf07d86947b1680828b328fe50c86bcf759e5be
alexandrup <alexandrup@ensemble.com>
2022-05-26 17:01:56 +0300

    Asserted Contact Us Success and Failure messages

--- cypress/integration/WebdriverUniversity/ContactUsPage.js
+++ cypress/integration/WebdriverUniversity/ContactUsPage.js
@@ -15,6 +15,7 @@ describe('Test Contact US form via WebdriverUni', () => {
     cy.get(this.SELECTORS.CONTACT_US_PAGE.EMAIL_FIELD).type(this.CONSTANTS.CONTACT_US_PAGE.EMAIL)
     cy.get(this.SELECTORS.CONTACT_US_PAGE.COMMENTS_FIELD).type(this.CONSTANTS.CONTACT_US_PAGE.COMMENTS)
     cy.get(this.SELECTORS.CONTACT_US_PAGE.SUBMIT_CTA).click()
+    cy.xpath(util.format(this.SELECTORS.CONTACT_US_SUCCESS_PAGE.SUCCESS_MESSAGE, this.CONSTANTS.CONTACT_US_SUCCESS_PAGE.SUCCESS_MESSAGE)).should('have.text', this.CONSTANTS.CONTACT_US_SUCCESS_PAGE.SUCCESS_MESSAGE)
   })
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function () {
@@ -22,5 +23,6 @@ describe('Test Contact US form via WebdriverUni', () => {
     cy.get(this.SELECTORS.CONTACT_US_PAGE.LAST_NAME_FIELD).type(this.CONSTANTS.CONTACT_US_PAGE.LAST_NAME),
     cy.get(this.SELECTORS.CONTACT_US_PAGE.COMMENTS_FIELD).type(this.CONSTANTS.CONTACT_US_PAGE.COMMENTS)
     cy.get(this.SELECTORS.CONTACT_US_PAGE.SUBMIT_CTA).click()
+    cy.get(this.SELECTORS.CONTACT_US_ERROR_PAGE.ERROR_MESSAGE_CONTAINER).should('include.text', this.CONSTANTS.CONTACT_US_ERROR_PAGE.ERROR_MESSAGE)
   })
 })
